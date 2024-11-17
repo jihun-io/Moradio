1c59a0c00853d56564d0ee7dd7e6bea9eeda1a19
김지훈 <kim@jihun.io>
2024-11-17 19:13:15 +0900

    로고 관리 상수 추가

---
+++ constants/stationsLogo.ts
@@ -0,0 +1,54 @@
+import bbsLogo from '../assets/images/stations/bbs.png';
+import cbsLogo from '../assets/images/stations/cbs.png';
+import cpbcLogo from '../assets/images/stations/cpbc.png';
+import ebsLogo from '../assets/images/stations/ebsfm.png';
+import febcLogo from '../assets/images/stations/febc.png';
+import kbs1Logo from '../assets/images/stations/kbs1.png';
+import kbs2Logo from '../assets/images/stations/kbs2.png';
+import kbs3Logo from '../assets/images/stations/kbs3.png';
+import kbs1fmLogo from '../assets/images/stations/kbs1fm.png';
+import kbs2fmLogo from '../assets/images/stations/kbs2fm.png';
+import kbshanminjokLogo from '../assets/images/stations/kbshanminjok.png';
+import kookbangLogo from '../assets/images/stations/kookbang.png';
+import kugakLogo from '../assets/images/stations/kugak.png';
+import mbcfm4uLogo from '../assets/images/stations/mbcfm4u.png';
+import mbcsfmLogo from '../assets/images/stations/mbcsfm.png';
+import sbslovefmLogo from '../assets/images/stations/sbslovefm.png';
+import sbspowerfmLogo from '../assets/images/stations/sbspowerfm.png';
+import tbsefmLogo from '../assets/images/stations/tbsefm.png';
+import tbsfmLogo from '../assets/images/stations/tbsfm.png';
+import ytnLogo from '../assets/images/stations/ytn.png';
+import tbnLogo from '../assets/images/stations/tbn.png';
+import obsLogo from '../assets/images/stations/obs.png';
+import ifmLogo from '../assets/images/stations/ifm.png';
+import afnLogo from '../assets/images/stations/afn.png';
+import wbsLogo from '../assets/images/stations/wbs.png';
+
+// 이미지 매핑 객체
+export const stationImages: {[key: string]: any} = {
+  bbs: bbsLogo,
+  cbs: cbsLogo,
+  cpbc: cpbcLogo,
+  ebs: ebsLogo,
+  febc: febcLogo,
+  kbs1radio: kbs1Logo,
+  kbs2radio: kbs2Logo,
+  kbs3radio: kbs3Logo,
+  kbs1fm: kbs1fmLogo,
+  kbs2fm: kbs2fmLogo,
+  kbshanminjok: kbshanminjokLogo,
+  kookbang: kookbangLogo,
+  kugak: kugakLogo,
+  mbcfm4u: mbcfm4uLogo,
+  mbcsfm: mbcsfmLogo,
+  sbslovefm: sbslovefmLogo,
+  sbspowerfm: sbspowerfmLogo,
+  tbsefm: tbsefmLogo,
+  tbsfm: tbsfmLogo,
+  ytn: ytnLogo,
+  tbn: tbnLogo,
+  obs: obsLogo,
+  ifm: ifmLogo,
+  afn: afnLogo,
+  wbs: wbsLogo,
+};
